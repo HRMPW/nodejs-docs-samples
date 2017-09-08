1a5adbb7c905c2a5c33cc1d84e66cbe083c3240c
Patrick Wolf <pwolf@cloudbees.com>
2017-09-08 08:38:14 -0700

    New serverless function for products and added cors package

--- functions/datastore/index.js
+++ functions/datastore/index.js
@@ -19,60 +19,21 @@ const Datastore = require('@google-cloud/datastore');
 
 // Instantiates a client
 const datastore = Datastore();
+const cors = require('cors')({origin: true});
 
 /**
  * Gets a Datastore key from the kind/key pair in the request.
  *
- * @param {object} requestData Cloud Function request data.
- * @param {string} requestData.key Datastore key string.
- * @param {string} requestData.kind Datastore kind.
+ * @param {string} requestKey The key in query string
  * @returns {object} Datastore key object.
  */
-function getKeyFromRequestData (requestData) {
-  if (!requestData.key) {
+function getKeyFromRequestData (requestKey) {
+  if (!requestKey) {
     throw new Error('Key not provided. Make sure you have a "key" property in your request');
   }
 
-  if (!requestData.kind) {
-    throw new Error('Kind not provided. Make sure you have a "kind" property in your request');
-  }
-
-  return datastore.key([requestData.kind, requestData.key]);
+  return datastore.key(["products", requestKey]);
 }
-
-/**
- * Creates and/or updates a record.
- *
- * @example
- * gcloud beta functions call set --data '{"kind":"Task","key":"sampletask1","value":{"description": "Buy milk"}}'
- *
- * @param {object} req Cloud Function request context.
- * @param {object} req.body The request body.
- * @param {string} req.body.kind The Datastore kind of the data to save, e.g. "Task".
- * @param {string} req.body.key Key at which to save the data, e.g. "sampletask1".
- * @param {object} req.body.value Value to save to Cloud Datastore, e.g. {"description":"Buy milk"}
- * @param {object} res Cloud Function response context.
- */
-exports.set = function set (req, res) {
-  // The value contains a JSON document representing the entity we want to save
-  if (!req.body.value) {
-    throw new Error('Value not provided. Make sure you have a "value" property in your request');
-  }
-
-  const key = getKeyFromRequestData(req.body);
-  const entity = {
-    key: key,
-    data: req.body.value
-  };
-
-  return datastore.save(entity)
-    .then(() => res.status(200).send(`Entity ${key.path.join('/')} saved.`))
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send(err);
-      return Promise.reject(err);
-    });
-};
 
 /**
  * Retrieves a record.
@@ -81,52 +42,26 @@ exports.set = function set (req, res) {
  * gcloud beta functions call get --data '{"kind":"Task","key":"sampletask1"}'
  *
  * @param {object} req Cloud Function request context.
- * @param {object} req.body The request body.
- * @param {string} req.body.kind The Datastore kind of the data to retrieve, e.g. "Task".
- * @param {string} req.body.key Key at which to retrieve the data, e.g. "sampletask1".
  * @param {object} res Cloud Function response context.
  */
-exports.get = function get (req, res) {
-  const key = getKeyFromRequestData(req.body);
+exports.products = function products (req, res) {
+  cors(req, res, () => {
+    const key = getKeyFromRequestData(req.query.term.toLowerCase());
 
-  return datastore.get(key)
-    .then(([entity]) => {
+    return datastore.get(key)
+      .then(([entity]) => {
       // The get operation will not fail for a non-existent entity, it just
       // returns null.
-      if (!entity) {
-        throw new Error(`No entity found for key ${key.path.join('/')}.`);
-      }
+        if (!entity) {
+          throw new Error(`No entity found for key ${key.path.join('/')}.`);
+        }
 
-      res.status(200).send(entity);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send(err);
-      return Promise.reject(err);
-    });
-};
-
-/**
- * Deletes a record.
- *
- * @example
- * gcloud beta functions call del --data '{"kind":"Task","key":"sampletask1"}'
- *
- * @param {object} req Cloud Function request context.
- * @param {object} req.body The request body.
- * @param {string} req.body.kind The Datastore kind of the data to delete, e.g. "Task".
- * @param {string} req.body.key Key at which to delete data, e.g. "sampletask1".
- * @param {object} res Cloud Function response context.
- */
-exports.del = function del (req, res) {
-  const key = getKeyFromRequestData(req.body);
-
-  // Deletes the entity
-  return datastore.delete(key)
-    .then(() => res.status(200).send(`Entity ${key.path.join('/')} deleted.`))
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send(err);
-      return Promise.reject(err);
+        res.status(200).send(entity.matches.map(JSON.parse));
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send(err);
+        return Promise.reject(err);
+      });
     });
 };
